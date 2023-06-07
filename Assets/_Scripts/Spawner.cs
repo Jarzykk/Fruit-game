b19f3afb8f9ea55cbb7e83d13abd91c2126cc506
Yaroslav Timoshenko <jarzykk@gmail.com>
2023-06-07 13:08:32 +0300

    Change apple prefub

---
+++ Assets/_Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _templateToSpawn;
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private float _spawnRate;
+
+    private float _elapsedTime = 0;
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+        if (_elapsedTime >= _spawnRate)
+        {
+            _elapsedTime = 0;
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        int spawnPointIndex = Random.Range(0, _spawnPoints.Length);
+        Instantiate(_templateToSpawn, _spawnPoints[spawnPointIndex]);
+    }
+}
